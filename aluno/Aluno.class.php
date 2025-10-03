8b05f4a9680aeb6c844d81c08674d7b8ed42297b
DEV_Romão <brunodossantosromao11@gmail.com>
2025-10-03 14:21:08 -0300

    add nome bruno

--- aluno/Aluno.class.php
+++ aluno/Aluno.class.php
@@ -1,83 +1,84 @@
 <?php
 class Aluno{
-    private $id;
-    private $rm;
-    private $nome;
-    private $email;
-    private $cpf;
-    private $pdo;
-    private $senha;
+    private $brunoId;
+    private $brunoRm;
+    private $brunoNome;
+    private $brunoEmail;
+    private $brunoCpf;
+    private $brunoPdo;
+    private $brunoSenha;
     
 
     public function conectar(){
-        $dns    = "mysql:dbname=EtimPwiiAluno;host=localhost";
-        $dbUser = "root";
-        $dbPass = "";
+        $brunoDns    = "mysql:dbname=EtimPwiiAluno;host=localhost";
+        $brunoDbUser = "root";
+        $brunoDbPass = "";
 
         try {
-            $this->pdo = new PDO($dns, $dbUser, $dbPass);
+            $this->brunoPdo = new PDO($brunoDns, $brunoDbUser, $brunoDbPass);
             return true;
-        } catch (\Throwable $th) {
+        } catch (\Throwable $brunoTh) {
             return false;
         }
     }
 
-    public function getId(){
-        return $this->id;
+    public function getBrunoId(){
+        return $this->brunoId;
     }
-    public function getRm(){
-        return $this->rm;
+    public function getBrunoRm(){
+        return $this->brunoRm;
     }
-    public function getNome(){
-        return $this->nome;
+    public function getBrunoNome(){
+        return $this->brunoNome;
     }
-    public function getEmail(){
-        return $this->email;
+    public function getBrunoEmail(){
+        return $this->brunoEmail;
     }
-    public function getCpf(){
-        return $this->cpf;
-    }
-
-    public function setRm($rm){
-        $this->rm = $rm ;
-    }
-    public function setNome($nome){
-        $this->nome = $nome ;
-    }
-    public function setEmail($email){
-        $this->email = $email ;
-    }
-    public function setCpf($cpf){
-        $this->cpf = $cpf ;
+    public function getBrunoCpf(){
+        return $this->brunoCpf;
     }
 
-    public function cadastrar($rm, $nome, $email, $cpf){
-        
-        $sql = "INSERT INTO aluno set rm = :r, nome = :n, email = :e, cpf = :c";
-        
-        $sql = $this->pdo->prepare($sql);
+    public function setBrunoRm($brunoRm){
+        $this->brunoRm = $brunoRm ;
+    }
+    public function setBrunoNome($brunoNome){
+        $this->brunoNome = $brunoNome ;
+    }
+    public function setBrunoEmail($brunoEmail){
+        $this->brunoEmail = $brunoEmail ;
+    }
+    public function setBrunoCpf($brunoCpf){
+        $this->brunoCpf = $brunoCpf ;
+    }
 
+    public function cadastrar($brunoRm, $brunoNome, $brunoEmail, $brunoCpf){
         
-        $sql-> bindValue(":r", $rm);
-        $sql-> bindValue(":n", $nome);
-        $sql-> bindValue(":e", $email);
-        $sql-> bindValue(":c", $cpf);
+        $brunoSql = "INSERT INTO aluno set rm = :r, nome = :n, email = :e, cpf = :c";
+        
+        $brunoSql = $this->brunoPdo->prepare($brunoSql);
 
         
-        return $sql->execute();
+        $brunoSql-> bindValue(":r", $brunoRm);
+        $brunoSql-> bindValue(":n", $brunoNome);
+        $brunoSql-> bindValue(":e", $brunoEmail);
+        $brunoSql-> bindValue(":c", $brunoCpf);
+
+        
+        return $brunoSql->execute();
     }
 
-    public function consultar($email){
-        $sql = "SELECT *FROM aluno WHERE email = :e";
-        $sql = $this->pdo->prepare($sql);
-        $sql-> bindValue(":e", $email);
-        $sql->execute();
+    public function consultar($brunoEmail){
+        $brunoSql = "SELECT *FROM aluno WHERE email = :e";
+        $brunoSql = $this->brunoPdo->prepare($brunoSql);
+        $brunoSql-> bindValue(":e", $brunoEmail);
+        $brunoSql->execute();
 
-        return $sql->rowCount() > 0;
+        return $brunoSql->rowCount() > 0;
         
     }
 
 
 }
+
 
 
